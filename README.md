277e6042e4c804181e3d713168060a79c67a28bc
Daniel Kończyk <daniel@konczyk.net>
2025-12-11 00:08:33 +0100

    Handle single patterns

--- README.md
+++ README.md
@@ -4,9 +4,32 @@ Simple grep clone written in Rust
 
 ## Usage
 
-Build binary
+Build project
 ```shell
 cargo build
 ```
 
+Run tests
+```shell
+cargo test
+```
+
+Execute
+```shell
+echo -n 'text' | ./target/debug/rgrep -E [PATTERN]
+```
+Program returns exit code 0 on match and 1 otherwise
+
+Supported patterns
+- string literals
+- \d - digits
+- \w - word characters
+- [abc] - positive character groups
+- [^abc] - negative character groups
+
 ## Examples
+
+Match single digit
+```shell
+echo -n 'text9' | ./target/debug/rgrep -E '\d'
+```
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,80 @@ use std::env;
 use std::io;
 use std::process;
 
+fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    if pattern.chars().count() == 1 {
+        input_line.contains(pattern)
+    } else if pattern == "\\d" {
+        input_line.chars().any(|x| x.is_ascii_digit())
+    } else if pattern == "\\w" {
+        input_line.chars().any(|x| x.is_ascii_alphabetic() || x.is_ascii_digit() || x == '_')
+    } else if pattern.starts_with("[^") && pattern.ends_with(']') {
+        let pat = &pattern[2..pattern.len() - 1];
+        input_line
+            .chars()
+            .any(|x| !pat.contains(x))
+    } else if pattern.starts_with('[') && pattern.ends_with(']') {
+        pattern[1..pattern.len() - 1]
+            .chars()
+            .any(|x| input_line.contains(x))
+    } else {
+        panic!("Unhandled pattern: {}", pattern)
+    }
+}
+
 fn main() {
     if env::args().nth(1).unwrap() != "-E" {
-        println!("Missing first argument to be '-E'");
+        println!("Missing first argument '-E'");
         process::exit(1);
     }
 
+    let pattern = env::args().nth(2).unwrap();
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    process::exit(0)
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn match_literal() {
+        assert_eq!(match_pattern("rust", "u"), true);
+        assert_eq!(match_pattern("rust", "a"), false);
+    }
+
+    #[test]
+    fn match_digit() {
+        assert_eq!(match_pattern("rust23p", "\\d"), true);
+        assert_eq!(match_pattern("rust", "\\d"), false);
+    }
+
+    #[test]
+    fn match_word() {
+        assert_eq!(match_pattern("rust", "\\w"), true);
+        assert_eq!(match_pattern("123", "\\w"), true);
+        assert_eq!(match_pattern("_", "\\w"), true);
+        assert_eq!(match_pattern("[", "\\w"), false);
+    }
+
+    #[test]
+    fn match_group() {
+        assert_eq!(match_pattern("rust", "[rs]"), true);
+        assert_eq!(match_pattern("[]", "[rs]"), false);
+        assert_eq!(match_pattern("rust", "[del]"), false);
+    }
+
+    #[test]
+    fn match_group_neg() {
+        assert_eq!(match_pattern("rust", "[^rs]"), true);
+        assert_eq!(match_pattern("[]", "[^rs]"), true);
+        assert_eq!(match_pattern("rust", "[^rstu]"), false);
+    }
 }
